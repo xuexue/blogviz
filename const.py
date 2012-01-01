2e44684f254f2496ba28736bdbe708dcae2c1956
Jeeyoung Kim <jeeyoungk@gmail.com>
2012-01-01 13:26:44 -0500

    Added sample code to export google analytics data.

---
+++ const.py
@@ -0,0 +1,6 @@
+# Author : Jeeyoung Kim
+
+# Api keys can be obtained from
+# https://code.google.com/apis/console/
+CLIENT_ID = '412634461797.apps.googleusercontent.com'
+CLIENT_SECRET = 'Jtb6ik3QDIX6igsSSkX2mIr1'
