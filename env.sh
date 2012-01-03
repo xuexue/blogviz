51f633d474fc8467ce49ca1df1bfca9033bbcd92
Jeeyoung Kim <jeeyoungk@gmail.com>
2012-01-03 15:34:27 -0500

    Added django-social-auth as git submodule.

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Run this script to set up environment variables required for django / python.
-export PYTHONPATH=`pwd`:$PYTHONPATH
+export PYTHONPATH=`pwd`:`pwd`/thirdparty/django-social-auth/:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE='settings'
